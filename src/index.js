226ca038b60b7a37e7ac38c9b8cc0d2e6ad55f63
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-06 13:57:33 -0500

    Create page switch on nav button press

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import "./styles.css";
 import { loadHome } from "./js/load-home";
+import { loadAbout } from "./js/load-about";
+import { loadMenu } from "./js/load-menu";
 
 export const contentDiv = document.querySelector("#content");
 
-loadHome()
+const restaurantDisplay = (function () {
+    const homeButton = document.querySelector("#home-button");
+    const menuButton = document.querySelector("#menu-button");
+    const aboutButton = document.querySelector("#about-button");
+
+    homeButton.addEventListener("click", loadHome);
+    menuButton.addEventListener("click", loadMenu);
+    aboutButton.addEventListener("click", loadAbout);
+
+    loadHome()
+})();
+
+
