bd61991e90423a57d5ccf2f791f749dbdeb90b44
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-07 11:36:31 -0500

    Add all current menu items to the menu page display

--- src/js/load-menu.js
+++ src/js/load-menu.js
@@ -7,4 +7,30 @@ export function loadMenu() {
     const menu = document.createElement("div")
     menu.id = "menu"
     contentDiv.append(menu)
+
+    const menuHeader = document.createElement("h1")
+    menuHeader.textContent = "Menu"
+    menu.append(menuHeader)
+
+    menuItems.forEach(item => {
+        let newItem = document.createElement("div")
+        newItem.classList.add("menu-item")
+        
+        let itemName = document.createElement("h1")
+        itemName.textContent = item.name
+        itemName.classList.add("menu-item-name")
+        newItem.append(itemName)
+
+        let itemPrice = document.createElement("p")
+        itemPrice.textContent = `Price: ${item.price}`
+        itemPrice.classList.add("menu-item-price")
+        newItem.append(itemPrice)
+
+        let itemDescription = document.createElement("p");
+        itemDescription.textContent = item.description;
+        itemDescription.classList.add("menu-item-description")
+        newItem.append(itemDescription)
+
+        menu.append(newItem)
+    })
 }
---
+++ src/js/menu/menu-items.js
@@ -0,0 +1,7 @@
+const newMenuItem = function(name, price, description) {
+    return {name, price, description}
+}
+
+export let menuItems = [
+    newMenuItem("Pizza", "$12.99", "It's just a pizza"),
+]
