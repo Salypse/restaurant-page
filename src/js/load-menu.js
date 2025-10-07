a658ca0bc74622874c111067842935eb35fe26f5
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-07 11:03:19 -0500

    Create restaurant menu skeleton

--- src/js/load-menu.js
+++ src/js/load-menu.js
@@ -1,5 +1,10 @@
 import { contentDiv } from "../index.js";
+import { menuItems } from "./menu/menu-items.js";
 
 export function loadMenu() {
-    contentDiv.innerHTML = "TEST"
+    contentDiv.innerHTML = "";
+
+    const menu = document.createElement("div")
+    menu.id = "menu"
+    contentDiv.append(menu)
 }
