e4525a143d3f7e90a1356290e668f8e3486a837a
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-07 11:47:54 -0500

    Add all other menu to fill menu display

--- src/js/menu/menu-items.js
+++ src/js/menu/menu-items.js
@@ -3,5 +3,10 @@ const newMenuItem = function(name, price, description) {
 }
 
 export let menuItems = [
+    newMenuItem("Seasonal Caramel Apple", "$4.99", "Dont order this, you might not be alive tommorow if you do"),
+    newMenuItem("Chicken Sandwhich", "$14.99", "It's definetly a sandwhich, whether its chicken is unknown"),
+    newMenuItem("Salad", "9.99", "This is a lame option for you to eat"),
+    newMenuItem("Fruit Assortment", "$9.99", "Comes with Strawberries, Bananas, Grapes, and something resembling an Orange"),
     newMenuItem("Pizza", "$12.99", "It's just a pizza"),
+    newMenuItem("Chocalate Chip Cookie", "$2.99", "Looking at this menu do you really think those are chocalate chips?"),
 ]
